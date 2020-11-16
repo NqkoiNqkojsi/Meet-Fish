dc0fd476ffbe7567ed3ad10cd6e2af800f6f3d19
Stelian Anonov <tanya_dzh@abv.bg>
2020-11-16 13:35:20 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- Fishing/datefilter.php
+++ Fishing/datefilter.php
@@ -1,8 +1,10 @@
 <?php
 if($_SERVER['REQUEST_URI']=="/index.php"){
+    console_log($_SERVER['REQUEST_URI'])
     $date=new DateTime();
     $date1=new DateTime();
     $date1->modify('+2 months');
+    console_log($date1);
 ?>
 <div class="dropdown" style="display: block;">
 	<button class="dropbtn" onclick="Show('myDropdown3')">По дата</button>
@@ -24,6 +26,7 @@ if($_SERVER['REQUEST_URI']=="/index.php"){
     $date=new DateTime();
     $date1=new DateTime();
     $date1->modify('+2 months');
+    console_log($date1);
 ?>
 <input type="text" data-field="datetime" data-view="Popup" data-min="<?php echo $date->format('d-m-Y'); ?>" data-max="<?php echo $date1->format('d-m-Y'); ?>" name="time"  readonly>
 <div id="dtBox1"></div>
