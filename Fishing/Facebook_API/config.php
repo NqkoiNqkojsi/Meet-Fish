fcd7d1ae9ee8566ae6c7f8614ade85cdf9543e1b
Stelian Anonov <tanya_dzh@abv.bg>
2020-11-29 00:24:53 +0200

    started the real api

--- Fishing/Facebook_API/config.php
+++ Fishing/Facebook_API/config.php
@@ -1,1 +1,13 @@
+<?php
+require_once('Facebook/Facebook/autoload.php');
+
+$fb = new \Facebook\Facebook([
+  'app_id' => '{198412948406802}',
+  'app_secret' => '{8f1a318c934afa4f244b3f48c37929b8}',
+  'default_graph_version' => 'v9.0',
+  'default_access_token' => 'EAAC0dJ1PAhIBAA6dCPzyAuWN3rBXegkGmYiOpXw3pjIjL5Ws2Mya3YuYZBNLc3kneBRGFDPGnCeJ41RtfJocm5isrGSxDAZCoR6OABEpaMKq2bu2kirYP5AcQuB220QDrQBh2Iyd64R3vmOorvHUTyN6FrCD9G5PsPD4kaJwZDZD' // optional
+]);
+
+?>
+
 
--- Fishing/Facebook_API/upload_to_facebook.php
+++ Fishing/Facebook_API/upload_to_facebook.php
@@ -1,3 +1,38 @@
-<?php
+﻿<?php
 //Used to upload the canvas to the facebook page
+//set the send variables
+
+$ime="";
+if(isset($_REQUEST["ime"])){
+	$ime=$_REQUEST["ime"];
+}
+$link="";
+if(isset($_REQUEST["link"])){
+	$ime=$_REQUEST["link"];
+}
+$link_img="";
+include("");
+include("config.php");
+$My_message=$ime."Ви кани на риболов на ".$link;
+$data = [
+  'message' => $My_message,
+  'source' => $fb->fileToUpload($link_img),
+];
+
+try {
+    // Returns a `Facebook\FacebookResponse` object
+    $response = $fb->post('/me/photos', $data, '{access-token}');
+}
+catch(Facebook\Exceptions\FacebookResponseException $e) {
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+}
+catch(Facebook\Exceptions\FacebookSDKException $e) {
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+}
+
+$graphNode = $response->getGraphNode();
+
+echo 'Photo ID: ' . $graphNode['id'];
 ?>
