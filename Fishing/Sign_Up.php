5af14a798cb132b666517877e7aa444423dab614
Stelian Anonov <tanya_dzh@abv.bg>
2020-11-28 10:30:52 +0200

    added more log

--- Fishing/Sign_Up.php
+++ Fishing/Sign_Up.php
@@ -114,6 +114,7 @@ if(isset($_POST["submit"])){
             $stmt->execute();
         }catch(Exception $e){
             console_log("Error: " . $e->getMessage());
+			console_log($sql);
         }
 	    include "email_verify.php";//send email verification
 		//header("location:../index.php");
