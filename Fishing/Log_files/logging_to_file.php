af6a45bcf5363c2a11475f02933acbf2da21d4b9
Stelian Anonov <tanya_dzh@abv.bg>
2020-12-04 20:56:45 +0200

    seching for mistakes

--- Fishing/Log_files/logging_to_file.php
+++ Fishing/Log_files/logging_to_file.php
@@ -18,8 +18,8 @@ function Log_file($log_msg, $log_filename){
             // create directory/folder uploads.
             mkdir($log_filename, 0777, true);
         }
-        $log_file_data = $log_filename.'/log_' . date('d-M-Y') . '.log';
-        file_put_contents($log_file_data, $log_msg . "\n", FILE_APPEND);
+        //$log_file_data = $log_filename.'/log_' . date('d-M-Y') . '.log';
+        //file_put_contents($log_file_data, $log_msg . "\n", FILE_APPEND);
     }
 }
 ?>
