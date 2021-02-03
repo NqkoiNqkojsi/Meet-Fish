069ae2ef3a4dba6c4d0af8f910dad3ad624d278c
NqkoiNqkojsi <tanya_dzh@abv.bg>
2021-02-03 03:17:04 +0200

    debug

--- Fishing/profile.php
+++ Fishing/profile.php
@@ -66,6 +66,7 @@ if(isset($_POST["submit"])){//Zapisvane na informaciq
 		$target_dir = "Img/User_Img";
 		$save_path=$target_dir.$row["Img_name"];
 		$file = $_FILES['my_file']['name'];
+		echo $_FILES['my_file']['error'];
 		$path = pathinfo($file);
 		$filename = $path['filename'];
 		$ext = $path['extension'];
