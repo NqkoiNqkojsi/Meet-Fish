3dc82b6c6358a6cf44129ff31cc20cf67c6086e0
Stel Nqkoi Edisikov <stelyo_2004@abv.bg>
2021-02-20 01:36:17 +0200

    started with the FB stuff

---
+++ Fishing/Facebook_API/delete_to_facebook.php
@@ -0,0 +1,47 @@
+﻿<?php
+//Used to upload the canvas to the facebook page
+//set the send variables
+$link="https://meetandfish.online/Fishing/offer.php?id=";
+if(isset($_REQUEST["link"])){
+	$link=$link.$_REQUEST["link"];
+}
+$ime="";
+if(isset($_REQUEST["ime"])){
+	$ime=$_REQUEST["ime"];
+}
+$mes="";
+if(isset($_REQUEST["mes"])){
+	$mes=$_REQUEST["mes"];
+}
+
+include("config.php");
+
+if (isset($AccessToken)) {
+    // Logged in.
+
+    $My_message=$ime." Ви кани на риболов ".$mes;
+    $data = [
+      'link' => $link,
+      'message' => $My_message
+    ];
+
+    try {
+        $response = $fb->post('/104356978172893/feed', $data, $AccessToken);
+    }
+    catch(Facebook\Exceptions\FacebookResponseException $e) {
+        $msg='Graph returned an error: '.$e->getMessage();
+        $error_string=__FILE__."****:".$msg;
+        error_log($error_string, 0, "../Log_files/API.log");
+        exit;
+    }
+    catch(Facebook\Exceptions\FacebookSDKException $e) {
+        $msg= 'Facebook SDK returned an error: '.$e->getMessage();
+        $error_string=__FILE__."****:".$msg;
+        error_log($error_string, 0, "../Log_files/API.log");
+        exit;
+    }
+    $graphNode = $response->getGraphNode();
+
+    echo 'Photo ID: ' . $graphNode['id'];
+}
+?>
