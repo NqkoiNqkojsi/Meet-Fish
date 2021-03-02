c04005cd3e8a48843d4aab38a674d510962a85de
Stel Nqkoi Edisikov <stelyo_2004@abv.bg>
2021-03-02 10:53:40 +0200

    debug

--- Fishing/Facebook_API/Start_File.php
+++ Fishing/Facebook_API/Start_File.php
@@ -4,9 +4,10 @@ if (array_key_exists('my_file', $_FILES)){
 	$path = pathinfo($_FILES['my_file']['name']);
 	$filename = $path['filename'];
 }
+echo "The place is ".$towns[$place];
 echo '<script type="text/javascript">',
 "Send_FC('".$new_date."', '".$_SESSION["user_Nname"]."', '".$filename."', '".
-$towns[intval($_SESSION["user_Plc"])]."', '".$last_id."');",
+$towns[$place]."', '".$last_id."');",
 '</script>'
 ;
 ?>
