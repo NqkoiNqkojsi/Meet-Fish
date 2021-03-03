846ead67fc7202fbe20396f00346f7106d91c1c1
Stel Nqkoi Edisikov <stelyo_2004@abv.bg>
2021-03-04 00:15:30 +0200

    workarounds

--- index.php
+++ index.php
@@ -57,6 +57,26 @@ include "Fishing/towns.php";
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	<style>
+		.Img_Cont {
+			position: relative;
+			width: 100%;
+			height: 300px;
+		}
+
+		.Img_In1 {
+			max-height: 95%;
+			max-width: 95%;
+			width: auto;
+			height: auto;
+			position: absolute;
+			top: 0;
+			bottom: 0;
+			left: 0;
+			right: 0;
+			margin: auto;
+		}
+	</style>
 </head>
 <body class="body">
 	<div class="navbar" id="navbar">
