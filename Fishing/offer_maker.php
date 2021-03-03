933f1dc028684661f859e2332415ccd21aff74a2
Stel Nqkoi Edisikov <stelyo_2004@abv.bg>
2021-03-04 00:03:01 +0200

    debug

--- Fishing/offer_maker.php
+++ Fishing/offer_maker.php
@@ -9,6 +9,7 @@ include "logging.php";
 		var xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function () {
 			if (this.readyState == 4 && this.status == 200) {
+				console.log("success");
 				setTimeout(() => { window.location = "http://meetandfish.online/index.php";}, 2000);
 			}	
 		};
