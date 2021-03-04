4f883956cac236445ccd25092dd67591373e7f6f
Stel Nqkoi Edisikov <stelyo_2004@abv.bg>
2021-03-05 01:06:33 +0200

    shit

--- Fishing/get_offer.php
+++ Fishing/get_offer.php
@@ -43,7 +43,6 @@ foreach($delete_id as $a){
 $br=0;
 foreach($delete_img as $img){
 	$path="Fishing/Img/Post_Img/".$img;
-	echo $path;
 	if (file_exists($path) && $path !="Fishing/Img/Post_Img/") {
 		unlink($path);
 	}
