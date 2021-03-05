c979224ae5f774c96269e38b7d25716e123bb1c2
Stel Nqkoi Edisikov <stelyo_2004@abv.bg>
2021-03-05 17:02:30 +0200

    even more logging

--- Fishing/Facebook_API/Make_Picture.php
+++ Fishing/Facebook_API/Make_Picture.php
@@ -38,7 +38,10 @@ function Error_Logging($name, $msg){
 
     fclose( $ifp );
 }
-Error_Logging("../Log_files/picture_making.txt", $link.":Going to Making picture")
+
+$msg=$link.": time=".$time." date=".$date." pic=".$prof_pic." id=".$link." mqsto=".$mqsto." ime=".$ime;
+Error_Logging("../Log_files/picture_making.txt", $link.":Going to Making picture");
+Error_Logging("../Log_files/picture_making.txt", );
 ?>
 <html>
 <head>
