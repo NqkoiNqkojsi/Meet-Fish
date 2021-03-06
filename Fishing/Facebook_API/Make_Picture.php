5646f79e0fe4ecff4fbc8a64fc71d98d9f609726
Stel Nqkoi Edisikov <stelyo_2004@abv.bg>
2021-03-06 02:43:20 +0200

    forgot to get the file name

--- Fishing/Facebook_API/Make_Picture.php
+++ Fishing/Facebook_API/Make_Picture.php
@@ -27,6 +27,13 @@ $ime="";
 if(isset($_REQUEST["ime"])){
 	$ime=$_REQUEST["ime"];
 }*/
+$filename="";
+if (array_key_exists('my_file', $_FILES)){
+	$path = pathinfo($_FILES['my_file']['name']);
+	$filename = $path['filename'].".".$path['extension'];
+}
+
+
 $date1=$new_date;
 $ime=$_SESSION["user_Nname"];
 $prof_pic="../Img/Post_Img/".$filename;
