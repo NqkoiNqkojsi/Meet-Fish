71b2ee00536236b477fcc6dd36ee3d16e62f8914
Stel Nqkoi Edisikov <stelyo_2004@abv.bg>
2021-03-06 23:06:23 +0200

    wtf

--- Fishing/Facebook_API/facebook_login.php
+++ Fishing/Facebook_API/facebook_login.php
@@ -5,7 +5,7 @@ function GetFacebookLoginUrl(){
 
 	$params = array(
 		'client_id' => APP_ID,
-		'riderect_uri' => RIDERECT_URI,
+		'redirect_uri' => RIDERECT_URI,
 		'state'=>APP_STATE,
 		'scope' => 'email',
 		'auth_type' => 'rerequest'
