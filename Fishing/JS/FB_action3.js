07c8814bbdfc538280ea2a2adc621a1255ffdd51
Stel Nqkoi Edisikov <stelyo_2004@abv.bg>
2021-03-06 16:59:15 +0200

    I hate this

--- Fishing/JS/FB_action3.js
+++ Fishing/JS/FB_action3.js
@@ -1,24 +1,22 @@
 function Login_FB(str) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             console.log("Success");
         }
     };
-    var xmlhttp = new XMLHttpRequest();
     console.log(str);
     xmlhttp.open("GET", "Fishing/Facebook_API/facebook_login.php", true);
     xmlhttp.send();
 }
 
 function Delete_FB(id) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
         }
     };
-    var xmlhttp = new XMLHttpRequest();
     console.log(id);
     xmlhttp.open("GET", "Fishing/Facebook_API/delete_to_facebook.php?id=" + id, true);
     xmlhttp.send();
