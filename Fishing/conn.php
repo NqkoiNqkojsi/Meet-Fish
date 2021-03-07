565b2ca4aee4b34561e78de860e328d069a0e1e3
Stel Nqkoi Edisikov <stelyo_2004@abv.bg>
2021-03-07 09:33:13 +0200

    wtf

--- Fishing/conn.php
+++ Fishing/conn.php
@@ -6,7 +6,7 @@ $dbname = "u157928248_fishing";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+echo "database conection";
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
